7846f29324e332ee9286fac35cffe78cdf9b4c78
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-09-23 16:27:18 -0300

    chore: rename `globals.css` to `global.css` in layout imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "@/lib/providers";
-import "./globals.css";
+import "./global.css";
 
 const inter = Inter({
   variable: "--font-inter",
